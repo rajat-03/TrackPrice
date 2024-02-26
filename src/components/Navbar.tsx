da59c5ae8e9064209f3aca68cda4b78d8fa1191f
Rajat Kumar <rajat9258@gmail.com>
2024-02-26 19:35:22 +0530

    initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+
+
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="w-full">
+      <nav className="nav">
+
+        {/* Logo portion start*/}
+        <Link href="/" className="flex items-center gap-1">
+
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+          <p className="nav-logo">
+            Track<span className="text-primary">Price</span>
+          </p>
+          
+        </Link>
+        {/* Logo portion end*/}
+
+        {/* Icons portion start*/}
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              width={28}
+              height={28}
+              alt={icon.alt}
+            />
+          ))}
+        </div>
+        {/* Icons portion end*/}
+
+      </nav>
+    </header>
   )
 }
 
