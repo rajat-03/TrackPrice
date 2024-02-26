bfc98c0d9b7f9680f666b2974ee125c0ca1e28d9
Rajat Kumar <rajat9258@gmail.com>
2024-02-26 20:29:41 +0530

    Add HeroCarousel and Searchbar components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,60 @@
+import HeroCarousel from "@/components/HeroCarousel"
+import Searchbar from "@/components/Searchbar"
+import Image from "next/image"
 
-export default function Home() {
+
+const Home = () => {
   return (
-   <>
-   <h1>Home</h1>
-   </>
-  );
+    <>
+      {/* first section */}
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary"> TrackPrice</span>
+            </h1>
+
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            </p>
+
+            <Searchbar />
+
+          </div>
+
+          <HeroCarousel />
+
+        </div>
+      </section>
+
+      {/* Trending section */}
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+
+          {['apple Iphone', 'Book', 'Sneakers'].map((product) => (
+            <div>{product}</div>
+          ))}
+
+
+        </div>
+
+      </section>
+    </>
+  )
 }
+
+export default Home
