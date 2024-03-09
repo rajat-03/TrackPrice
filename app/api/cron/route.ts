9a671333da42396bc08271e04b774a8cb44c2443
Rajat Kumar <rajat9258@gmail.com>
2024-03-10 00:04:03 +0530

    Remove unused constants and imports in cron route

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,10 +6,6 @@ import {Product} from "../../../lib/models/product.model";
 import { scrapeAmazonProduct } from "../../../lib/scraper";
 import { generateEmailBody, sendEmail } from "../../../lib/nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 5 minutes
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 export async function GET(request: Request) {
   try {
      await connectToDB();
